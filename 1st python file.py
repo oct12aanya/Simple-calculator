e382af4f480ca1a9d2798a5df26d267840b3a5ae
oct12aanya <125890573+oct12aanya@users.noreply.github.com>
2023-02-20 19:05:52 +0530

    Add files via upload

---
+++ 1st python file.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+# print(10 + 5)
+# print("6 - 7")
+# print(6 - 7)
+print("Simple calculator")
+x = 2
+y = 10
+sum = x + y
+difference = x - y
+multiplication = x * y
+division = x / y
+remainder = x % y
+print('the sum of x and y is: ',sum)
+print('the difference of x and y is:',difference)
+print('the multiplication of x and y is:',multiplication)
+print('the division of x and y is:',division)
+print('the remainder of x and y is:',remainder)
